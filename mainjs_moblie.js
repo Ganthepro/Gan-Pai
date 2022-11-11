535f28b563a5d7987e270c48d9563be7b03c4890
Tamtikorn <gan_zaza_12@hotmail.com>
2022-11-11 20:47:14 +0700

    new

--- mainjs_moblie.js
+++ mainjs_moblie.js
@@ -1,3 +1,66 @@
+function getPara(loc) {
+    let para = new URLSearchParams(window.location.search)
+    return para.get(loc)
+}
+
+if (localStorage.getItem('name') == null) {
+    console.log(localStorage.getItem('name'))
+    localStorage.setItem('all', '[]');localStorage.setItem('name','')
+}
+
+let local_name = localStorage.getItem('name')
+console.log(local_name)
+const loc = getPara('loc')
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx5BSfMMYu-ng_n-V14coSZ8GVOVr2dHTH7I0QVnWYKyXpmT0gXX8EyQaXyN1S1mLyq/exec'
+let cookies = document.cookie.split(';').map(cookie => cookie.split('=')).reduce((accumulator, [key, value]) => ({ ...accumulator, [key.trim()]: decodeURIComponent(value) }), {});
+const form = document.forms['restaurants']
+document.getElementById('detail').innerHTML = 'ลงทะเบียนครั้งแรก ' + '(' + loc + ')'
+// console.log(all_loc)
+if (local_name == '') {
+    form.addEventListener('submit', e => {
+        localStorage.setItem('name',document.getElementById('user').value)
+        e.preventDefault()
+        location.reload()
+    })
+} else if (local_name != '') {
+    document.getElementById("mt-3").click();
+    const d = new Date()
+    document.getElementById('loc').defaultValue = loc
+    document.getElementById('user').defaultValue = local_name 
+    document.getElementById('dt').defaultValue = d.getDate() + '/' + d.getMonth() + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => restaurants.reset())
+        .catch(error => console.error('Error!', error.message))
+    const forums = document.getElementById('form')
+    const br = document.createElement('br')
+    const hr = document.createElement('hr')
+    const p = document.createElement('p')
+    const text = document.createElement('h1')
+    const background = document.getElementById('personal')
+    const clear_btn = document.createElement('button')
+    const view_btn = document.createElement('button');const a_btn = document.createElement('a') 
+    document.getElementById('detail').innerHTML = 'Log In'
+    forums.style.opacity = '0'
+    text.innerHTML = 'Finish!!!'; p.innerHTML = 'คุณถูกบันทึกเข้าระบบแล้ว'; p.style.fontSize = '20px'
+    text.style.position = 'absolute'; text.style.font = 'bold'; text.style.fontSize = '72px'
+    text.style.top = '40%'; text.style.left = '50%'; hr.style.marginTop = '10px'
+    text.style.transform = 'translate(-50%,-40%)';view_btn.setAttribute('class','btn-primary')
+    clear_btn.innerText = 'Clear'; clear_btn.setAttribute('onclick', 'clearStorage()'); clear_btn.setAttribute('id','clearBtn')
+    a_btn.innerHTML = 'View Data';a_btn.setAttribute('id','a-btn');view_btn.style.position = 'absolute';a_btn.setAttribute('href','https://docs.google.com/spreadsheets/d/1IUQKYDa5rHDI520jWxNBzVTOV1pWX4MDCXPNQr5dEPE/edit#gid=1356909475')
+    view_btn.style.bottom = '-80px';view_btn.style.left = '50%';view_btn.style.transform = 'translate(-50%,0)';view_btn.appendChild(a_btn);
+    text.appendChild(view_btn)
+    text.appendChild(br)
+    text.appendChild(p)
+    text.appendChild(hr)
+    text.appendChild(clear_btn)
+    background.appendChild(text)
+}
+function clearStorage() {
+    localStorage.setItem('name','')
+    location.reload()
+}
+
 var break_key = 0;
 var break_key1 = 0;
 const home = document.getElementById('button-1');
@@ -17,8 +80,8 @@ if (width < 984 && break_key === 0) {
     new_a_home.setAttribute('href', '/tamtikorn_portfolio');
     new_a_github.setAttribute('id', 'new_a_github');
     new_a_github.setAttribute('href', 'https://github.com/Ganthepro/Ganthepro/tree/main/project');
-    new_a_home_img.setAttribute('src','home-pngrepo-com.png');
-    new_a_github_img.setAttribute('src','github-pngrepo-com (1).png');
+    new_a_home_img.setAttribute('src', 'home-pngrepo-com.png');
+    new_a_github_img.setAttribute('src', 'github-pngrepo-com (1).png');
     new_a_home_img.style.width = '25px';
     new_a_home_img.style.height = '25px';
     new_a_home_img.style.marginRight = '25px';
@@ -35,33 +98,26 @@ else if (width > 984 && break_key1 === 0) {
     break_key1 += 1
     break_key += 1
 }
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
     var width = window.innerWidth;
     const new_a_home = document.createElement('a');
     const new_a_home_img = document.createElement('img');
     const new_a_github = document.createElement('a');
     const new_a_github_img = document.createElement('img');
     const a_link = document.getElementById('a-link');
-	if (width < 768 && break_key === 0) {
+    const svg1 = document.getElementById('svg-1');
+    const svg2 = document.getElementById('svg-2');
+    if (width < 768 && break_key === 0) {
         home.remove();
         github.remove();
         new_a_home.setAttribute('id', 'new_a_home');
         new_a_home.setAttribute('href', '/tamtikorn_portfolio');
         new_a_github.setAttribute('id', 'new_a_github');
         new_a_github.setAttribute('href', 'https://github.com/Ganthepro/Ganthepro/tree/main/project');
-        new_a_home_img.setAttribute('src','home-pngrepo-com.png');
-        new_a_github_img.setAttribute('src','github-pngrepo-com (1).png');
-        // new_a_home_img.style.width = '25px';
-        // new_a_home_img.style.height = '25px';
-        // new_a_home_img.style.marginRight = '25px';
-        // new_a_github_img.style.width = '25px';
-        // new_a_github_img.style.height = '25px';
-        // a_link.style.marginRight = '10px'
-        // new_a_home.appendChild(new_a_home_img);
-        // new_a_github.appendChild(new_a_github_img);
-        // a_link.appendChild(new_a_home);
-        // a_link.appendChild(new_a_github);
-        // h3get.remove();
+        new_a_home_img.setAttribute('src', 'home-pngrepo-com.png');
+        new_a_github_img.setAttribute('src', 'github-pngrepo-com (1).png');
+        svg1.style.width = toString(width) + 'px'
+        svg2.style.width = toString(width) + 'px'
         break_key += 1
         break_key1 -= 1
     }
@@ -71,12 +127,12 @@ window.addEventListener('resize', function() {
         a_home.remove()
         a_github.remove()
         const button_1 = document.createElement('button');
-        button_1.setAttribute('id','button-1')
-        button_1.setAttribute('onclick',"window.location.href = '/tamtikorn_portfolio'")
+        button_1.setAttribute('id', 'button-1')
+        button_1.setAttribute('onclick', "window.location.href = '/tamtikorn_portfolio'")
         button_1.innerHTML = 'Home'
         const button_2 = document.createElement('button');
-        button_2.setAttribute('id','button-2')
-        button_2.setAttribute('onclick',"window.location.href = 'https://github.com/Ganthepro/Ganthepro/tree/main/project'")
+        button_2.setAttribute('id', 'button-2')
+        button_2.setAttribute('onclick', "window.location.href = 'https://github.com/Ganthepro/Ganthepro/tree/main/project'")
         button_2.innerHTML = 'GitHub'
         a_link.appendChild(button_1);
         a_link.appendChild(button_2);
@@ -85,174 +141,3 @@ window.addEventListener('resize', function() {
         break_key1 += 1
     }
 })
-
-function blur_all1() {
-    const allElements = document.querySelectorAll('a');
-    const h6_allElements = document.querySelectorAll('a > h6');
-    const allElements2 = document.querySelectorAll('body > *');
-    for (const element of allElements) {
-        // console.log(element.className)
-        if (element.className != 'but-1') {
-            element.style.filter = 'blur(4px)';
-        } else {
-            const main = document.getElementById('main1');
-            const sub = document.getElementById('sub1');
-            main.style.borderBottom = '4px solid rgb(255, 209, 63)';
-            sub.style.color = 'black';
-        }
-    }
-    for (const element of allElements2) {
-        // console.log(element.className)
-        if (element.id != 'button-container') {
-            element.style.filter = 'blur(4px)';
-        }
-    } 
-    for (const element of h6_allElements) {
-        element.style.transition = 'border-bottom 1s'   
-    }
-}
-function blur_all2() {
-    const allElements = document.querySelectorAll('a');
-    const h6_allElements = document.querySelectorAll('a > h6');
-    const allElements2 = document.querySelectorAll('body > *');
-    for (const element of allElements) {
-        // console.log(element.className)
-        if (element.className != 'but-2') {
-            element.style.filter = 'blur(4px)';
-        } else {
-            const main = document.getElementById('main2');
-            const sub = document.getElementById('sub2');
-            main.style.borderBottom = '4px solid rgb(255, 209, 63)';
-            sub.style.color = 'black';
-        }
-    }
-    for (const element of allElements2) {
-        // console.log(element.className)
-        if (element.id != 'button-container') {
-            element.style.filter = 'blur(4px)';
-        }
-    }
-    for (const element of h6_allElements) {
-        element.style.transition = 'border-bottom 1s'   
-    }
-}
-function blur_all3() {
-    const allElements = document.querySelectorAll('a');
-    const h6_allElements = document.querySelectorAll('a > h6');
-    const allElements2 = document.querySelectorAll('body > *');
-    for (const element of allElements) {
-        // console.log(element.className)
-        if (element.className != 'but-3') {
-            element.style.filter = 'blur(4px)';
-        } else {
-            const main = document.getElementById('main3');
-            const sub = document.getElementById('sub3');
-            main.style.borderBottom = '4px solid rgb(255, 209, 63)';
-            sub.style.color = 'black';
-        }
-    }
-    for (const element of allElements2) {
-        // console.log(element.className)
-        if (element.id != 'button-container') {
-            element.style.filter = 'blur(4px)';
-        }
-    }
-    for (const element of h6_allElements) {
-        element.style.transition = 'border-bottom 1s'   
-    }
-}
-function blur_all4() {
-    const allElements = document.querySelectorAll('a');
-    const h6_allElements = document.querySelectorAll('a > h6');
-    const allElements2 = document.querySelectorAll('body > *');
-    for (const element of allElements) {
-        // console.log(element.className)
-        if (element.className != 'but-4') {
-            element.style.filter = 'blur(4px)';
-        } else {
-            const main = document.getElementById('main4');
-            const sub = document.getElementById('sub4');
-            main.style.borderBottom = '4px solid rgb(255, 209, 63)';
-            sub.style.color = 'black';
-        }
-    }
-    for (const element of allElements2) {
-        // console.log(element.className)
-        if (element.id != 'button-container') {
-            element.style.filter = 'blur(4px)';
-        }
-    }
-    for (const element of h6_allElements) {
-        element.style.transition = 'border-bottom 1s'   
-    }
-}
-
-function blur_all5() {
-    const allElements = document.querySelectorAll('a');
-    const h6_allElements = document.querySelectorAll('a > h6');
-    const allElements2 = document.querySelectorAll('body > *');
-    for (const element of allElements) {
-        // console.log(element.className)
-        if (element.className != 'but-5') {
-            element.style.filter = 'blur(4px)';
-        } else {
-            const main = document.getElementById('main5');
-            const sub = document.getElementById('sub5');
-            main.style.borderBottom = '4px solid rgb(255, 209, 63)';
-            sub.style.color = 'black';
-        }
-    }
-    for (const element of allElements2) {
-        // console.log(element.className)
-        if (element.id != 'button-container') {
-            element.style.filter = 'blur(4px)';
-        } 
-    }
-    for (const element of h6_allElements) {
-        element.style.transition = 'border-bottom 1s'   
-    }
-}
-function blur_all6() {
-    const allElements = document.querySelectorAll('a');
-    const h6_allElements = document.querySelectorAll('a > h6');
-    const allElements2 = document.querySelectorAll('body > *');
-    for (const element of allElements) {
-        // console.log(element.className)
-        if (element.className != 'but-6') {
-            element.style.filter = 'blur(4px)';
-        } else {
-            const main = document.getElementById('main6');
-            const sub = document.getElementById('sub6');
-            main.style.borderBottom = '4px solid rgb(255, 209, 63)';
-            sub.style.color = 'black';
-        }
-    }
-    for (const element of allElements2) {
-        // console.log(element.className)
-        if (element.id != 'button-container') {
-            element.style.filter = 'blur(4px)';
-        }
-    }
-    for (const element of h6_allElements) {
-        element.style.transition = 'border-bottom 1s'   
-    }
-}
-
-function clear_blur_all() {
-    const allElements = document.querySelectorAll('body > *');
-    const a_allElements = document.querySelectorAll('a');
-    const h6_allElements = document.querySelectorAll('a > h6');
-    const p_allElements = document.querySelectorAll('a > p');
-    for (const element of allElements) {
-        element.style.filter = 'blur(0px)';
-    }
-    for (const element of a_allElements) {
-        element.style.filter = 'blur(0px)';
-    }
-    for (const element of h6_allElements) {
-        element.style.borderBottom = '0.1px solid white';
-    }
-    for (const element of p_allElements) {
-        element.style.color = 'white';
-    } 
-}
